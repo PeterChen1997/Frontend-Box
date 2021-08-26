a1d06f5de5f05a705abacd67f908198c15d52989
chenayangbj01 <chenyangbj01@fenbi.com>
2021-08-26 21:40:47 +0800

    feat: add leetcode

---
+++ docs/nav.02.算法/02-leetcode/4.LinkedList/206.反转链表.ts
@@ -0,0 +1,69 @@
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ 
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+示例 2：
+
+
+输入：head = [1,2]
+输出：[2,1]
+示例 3：
+
+输入：head = []
+输出：[]
+ 
+
+提示：
+
+链表中节点的数目范围是 [0, 5000]
+-5000 <= Node.val <= 5000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+// 反转链表：右左右右
+function reverseList(head: ListNode | null): ListNode | null {
+    if (!head) {
+        return null;
+    }
+
+    let temp = head;
+    let next = null;
+    let prev = null;
+    while (temp) {
+        next = temp.next;
+        temp.next = prev;
+        prev = temp;
+        temp = next;
+    }
+
+    return head;
+}
