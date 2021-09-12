3153c0702b931964c1e855825d7d33af19171a50
chenayangbj01 <chenyangbj01@fenbi.com>
2021-09-12 10:41:45 +0800

    feat: add leetcode

---
+++ docs/nav.02.算法/02-leetcode/6.Tree/102. 二叉树的层序遍历.ts
@@ -0,0 +1,73 @@
+/**
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+ 
+
+示例：
+二叉树：[3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层序遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+    if (!root) {
+        return [];
+    }
+
+    let temp = [root];
+    let res: number[][] = [];
+
+    while (temp.length) {
+        let curLayer = [...temp];
+        temp = [];
+        let curRes = [];
+        for (let item of curLayer) {
+            curRes.push(item.val);
+            item.left && temp.push(item.left);
+            item.right && temp.push(item.right);
+        }
+        res.push(curRes);
+    }
+
+    return res;
+}
